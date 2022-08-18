ad622feb5f4005ceb09cbc00e541cb6726962371
Yashwanth Bharadwaj Vedula <91293705+Yashu2699@users.noreply.github.com>
2022-08-18 20:49:37 +0530

    Add files via upload

---
+++ assignment3 - PARTICLE FILTER COURSE/assignment3-3.py
@@ -0,0 +1,63 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import random as r
+
+
+class Robot:
+    def __init__(self, pos):
+        self.pos = pos
+        self.pole_dist = -100
+        self.max_measurement = 3
+
+    # Measurement is perfectly accurate even though we are assuming it isn't.
+    def measure(self, poles):
+        ### START STUDENT CODE
+        # Set self.pole_dist to the distance to the closest pole.
+        potential_detected_poles = []
+        for pole in poles:
+            diff = pole - self.pos
+            if diff > 0 and diff < self.max_measurement:
+                potential_detected_poles += [diff]
+        if len(potential_detected_poles) == 0:
+            self.pole_dist = -100
+        else:
+            self.pole_dist = min(potential_detected_poles)
+        ### END STUDENT CODE
+
+class Particle(Robot):
+    def __init__(self, pos):
+        Robot.__init__(self, pos)
+
+
+
+poles = [1, 10]
+particle = Particle(0.1)
+particle.measure(poles)
+print("Output Should Be 0.9")
+print("You Calculated: " + str(round(particle.pole_dist, 1)))
+print()
+
+particle.pos = 11
+particle.measure(poles)
+print("Output Should Be -100")
+print("You Calculated: " + str(round(particle.pole_dist, 1)))
+print()
+
+particle.pos = 6.9
+particle.measure(poles)
+print("Output Should Be -100")
+print("You Calculated: " + str(round(particle.pole_dist, 1)))
+print()
+
+particle.pos = 7.1
+particle.measure(poles)
+print("Output Should Be 2.9")
+print("You Calculated: " + str(round(particle.pole_dist, 1)))
+print()
+
+particle.pos = 9.5
+particle.measure(poles)
+print("Output Should Be 0.5")
+print("You Calculated: " + str(round(particle.pole_dist, 1)))
+print()
